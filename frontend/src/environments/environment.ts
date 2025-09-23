45884f19491dd55e51310422d39d2dea18c8bc10
Serigne Modou Bousso Ndiaye <ndiaye.serigne-modou-bousso@ugb.edu.sn>
2025-09-23 14:29:46 +0000

    changement api env prod

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://api.barhamienne-transport.com'
-  apiUrl: 'https://barhamienne-transport-laravel.smbn.website'
+  apiUrl: 'https://api.barhamienne-transport.com'
+  // apiUrl: 'https://barhamienne-transport-laravel.smbn.website'
 };
